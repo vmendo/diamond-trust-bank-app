bc1ecb484f294588ead22b97c349a513ebbcad35
Oracle Public Cloud User <opc@vm-jnb-cicd-dev2uat-01.npwls.vcnjnbnonprodap.oraclevcn.com>
2025-11-26 13:57:06 +0000

    feat: adding changelogs to the dist folder

---
+++ dist/releases/1.0/changes/release_init_1.0/wksp_wsdtbdev/indexes/sp_account_documents_a_indx_1.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+-- changeset WKSP_WSDTBDEV:1764165424558 stripComments:false  logicalFilePath:release_init_1.0/wksp_wsdtbdev/indexes/sp_account_documents_a_indx_1.sql
+-- sqlcl_snapshot src/database/wksp_wsdtbdev/indexes/sp_account_documents_a_indx_1.sql:null:eaa2e3c5d18032d35b1a2c6d1caebb9e2b5a1c1e:create
+
+create index wksp_wsdtbdev.sp_account_documents_a_indx_1 on
+    wksp_wsdtbdev.sp_account_documents (
+        account_id
+    );
+
