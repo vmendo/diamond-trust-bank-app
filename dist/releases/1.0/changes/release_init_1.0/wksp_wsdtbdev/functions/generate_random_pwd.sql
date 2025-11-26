d236e0bcd8c583e431ec80267ced95130e67ef23
Victor Mendo <vmendo@users.noreply.github.com>
2025-11-26 15:21:30 +0000

    feat: adding changelogs to the dist folder

---
+++ dist/releases/1.0/changes/release_init_1.0/wksp_wsdtbdev/functions/generate_random_pwd.sql
@@ -0,0 +1,12 @@
+-- liquibase formatted sql
+-- changeset WKSP_WSDTBDEV:1764170488023 stripComments:false  logicalFilePath:release_init_1.0/wksp_wsdtbdev/functions/generate_random_pwd.sql
+-- sqlcl_snapshot src/database/wksp_wsdtbdev/functions/generate_random_pwd.sql:null:cbeb85deecc1efdd97489a874b556d1a5c4c9729:create
+
+create or replace function wksp_wsdtbdev.generate_random_pwd return varchar2 is
+    pwdstring varchar2(255);
+begin
+    pwdstring := dbms_random.string('x', 10);
+    return ( pwdstring );
+end;
+/
+
