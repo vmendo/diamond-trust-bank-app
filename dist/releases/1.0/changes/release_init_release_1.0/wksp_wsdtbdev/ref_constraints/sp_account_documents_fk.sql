bcdb9f6f3510cff48a28a0ab3483a7e726338d9a
Oracle Public Cloud User <opc@vm-jnb-cicd-dev2uat-01.npwls.vcnjnbnonprodap.oraclevcn.com>
2025-11-26 13:43:23 +0000

    feat: adding changelogs to the dist folder

---
+++ dist/releases/1.0/changes/release_init_release_1.0/wksp_wsdtbdev/ref_constraints/sp_account_documents_fk.sql
@@ -0,0 +1,11 @@
+-- liquibase formatted sql
+-- changeset WKSP_WSDTBDEV:1764164601660 stripComments:false  logicalFilePath:release_init_release_1.0/wksp_wsdtbdev/ref_constraints/sp_account_documents_fk.sql
+-- sqlcl_snapshot src/database/wksp_wsdtbdev/ref_constraints/sp_account_documents_fk.sql:null:bbac56a8beadca6a01611ff9875cfe66cb4ef7fe:create
+
+alter table wksp_wsdtbdev.sp_account_documents
+    add constraint sp_account_documents_fk
+        foreign key ( account_id )
+            references wksp_wsdtbdev.sp_accounts ( id )
+                on delete cascade
+        enable;
+
---
+++ dist/releases/1.0/changes/release_init_release_1.0/wksp_wsdtbdev/triggers/sp_uploaded_files_type_biu.sql
@@ -0,0 +1,27 @@
+-- liquibase formatted sql
+-- changeset WKSP_WSDTBDEV:1764164602315 stripComments:false  logicalFilePath:release_init_release_1.0/wksp_wsdtbdev/triggers/sp_uploaded_files_type_biu.sql
+-- sqlcl_snapshot src/database/wksp_wsdtbdev/triggers/sp_uploaded_files_type_biu.sql:null:a0c043fdb779d88da9e2d2fc41119a7e09cbe5fb:create
+
+create or replace editionable trigger wksp_wsdtbdev.sp_uploaded_files_type_biu before
+    insert or update on wksp_wsdtbdev.sp_uploaded_files_type
+    for each row
+begin
+    :new.updated_on := sysdate;
+    :new.updated_by := coalesce(
+        sys_context('APEX$SESSION', 'APP_USER'),
+        user
+    );
+    if inserting then
+        :new.row_version := 1;
+        :new.created_on := :new.updated_on;
+        :new.created_by := :new.updated_by;
+    elsif updating then
+        :new.row_version := nvl(:old.row_version,
+                                0) + 1;
+    end if;
+
+end sp_uploaded_files_type_biu;
+/
+
+alter trigger wksp_wsdtbdev.sp_uploaded_files_type_biu enable;
+
