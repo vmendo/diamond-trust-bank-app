38cfbf6bd606731a502742d960ad8d8f7b9549e8
Victor Mendo <vmendo@users.noreply.github.com>
2025-11-27 09:58:10 +0000

    feat: committing changelogs in the dist folder for release 1.0

---
+++ dist/releases/1.0/changes/release_init_1.0/wksp_wsdtbdev/ref_constraints/sp_account_details_fk.sql
@@ -0,0 +1,11 @@
+-- liquibase formatted sql
+-- changeset WKSP_WSDTBDEV:1764237355084 stripComments:false  logicalFilePath:release_init_1.0/wksp_wsdtbdev/ref_constraints/sp_account_details_fk.sql
+-- sqlcl_snapshot src/database/wksp_wsdtbdev/ref_constraints/sp_account_details_fk.sql:null:d376e96f4f2cc4713cd9263c4a8705e58b5d0439:create
+
+alter table sp_account_details
+    add constraint sp_account_details_fk
+        foreign key ( account_id )
+            references sp_accounts ( id )
+                on delete cascade
+        enable;
+
---
+++ dist/releases/1.0/changes/release_init_1.0/wksp_wsdtbdev/triggers/sp_account_documents_biu.sql
@@ -0,0 +1,27 @@
+-- liquibase formatted sql
+-- changeset WKSP_WSDTBDEV:1764237355662 stripComments:false  logicalFilePath:release_init_1.0/wksp_wsdtbdev/triggers/sp_account_documents_biu.sql
+-- sqlcl_snapshot src/database/wksp_wsdtbdev/triggers/sp_account_documents_biu.sql:null:4df6e20a2972271cfd66ffc7783bcb0081fa9116:create
+
+create or replace editionable trigger sp_account_documents_biu before
+    insert or update on sp_account_documents
+    for each row
+begin
+    :new.updated_on := sysdate;
+    :new.updated_by := coalesce(
+        sys_context('APEX$SESSION', 'APP_USER'),
+        user
+    );
+    if inserting then
+        :new.row_version := 1;
+        :new.created_on := :new.updated_on;
+        :new.created_by := :new.updated_by;
+    elsif updating then
+        :new.row_version := nvl(:old.row_version,
+                                0) + 1;
+    end if;
+
+end sp_account_documents_biu;
+/
+
+alter trigger sp_account_documents_biu enable;
+
