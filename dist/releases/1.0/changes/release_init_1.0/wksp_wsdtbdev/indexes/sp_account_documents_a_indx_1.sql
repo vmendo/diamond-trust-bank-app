dc1b093e7f1ecd81e97683eba94a950a27bd3d50
Victor Mendo <vmendo@users.noreply.github.com>
2025-11-27 10:14:40 +0000

    feat: committing changelogs in the dist folder for release 1.0

---
+++ dist/releases/1.0/changes/release_init_1.0/wksp_wsdtbdev/indexes/sp_account_documents_a_indx_1.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+-- changeset WKSP_WSDTBDEV:1764238464060 stripComments:false  logicalFilePath:release_init_1.0/wksp_wsdtbdev/indexes/sp_account_documents_a_indx_1.sql
+-- sqlcl_snapshot src/database/wksp_wsdtbdev/indexes/sp_account_documents_a_indx_1.sql:null:ce0d9b9ec2b35ada0800e46050be4b23f000a2cd:create
+
+create index sp_account_documents_a_indx_1 on
+    sp_account_documents (
+        account_id
+    );
+
---
+++ dist/releases/1.0/changes/release_init_1.0/wksp_wsdtbdev/triggers/sp_account_details_biu.sql
@@ -0,0 +1,27 @@
+-- liquibase formatted sql
+-- changeset WKSP_WSDTBDEV:1764238464826 stripComments:false  logicalFilePath:release_init_1.0/wksp_wsdtbdev/triggers/sp_account_details_biu.sql
+-- sqlcl_snapshot src/database/wksp_wsdtbdev/triggers/sp_account_details_biu.sql:null:edc9ea2b1492fbcaf57f022218ddb9368b75a31d:create
+
+create or replace editionable trigger sp_account_details_biu before
+    insert or update on sp_account_details
+    for each row
+begin
+    :new.updated_on := sysdate;
+    :new.updated_by := coalesce(
+        sys_context('APEX$SESSION', 'APP_USER'),
+        user
+    );
+    if inserting then
+        :new.row_version := 1;
+        :new.created_on := :new.updated_on;
+        :new.created_by := :new.updated_by;
+    elsif updating then
+        :new.row_version := nvl(:old.row_version,
+                                0) + 1;
+    end if;
+
+end sp_account_details_biu;
+/
+
+alter trigger sp_account_details_biu enable;
+
