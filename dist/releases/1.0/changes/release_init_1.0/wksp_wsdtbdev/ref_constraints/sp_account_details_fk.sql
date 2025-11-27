5633a4fff80844f675b363cf75407962081d0700
Victor Mendo <vmendo@users.noreply.github.com>
2025-11-27 08:28:26 +0000

    feat: committing changelogs in the dist folder for release 1.0

---
+++ dist/releases/1.0/changes/release_init_1.0/wksp_wsdtbdev/ref_constraints/sp_account_details_fk.sql
@@ -0,0 +1,11 @@
+-- liquibase formatted sql
+-- changeset WKSP_WSDTBDEV:1764232084952 stripComments:false  logicalFilePath:release_init_1.0/wksp_wsdtbdev/ref_constraints/sp_account_details_fk.sql
+-- sqlcl_snapshot src/database/wksp_wsdtbdev/ref_constraints/sp_account_details_fk.sql:null:9a2cd8666a6be467a7dc10982e1546db6261639b:create
+
+alter table wksp_wsdtbdev.sp_account_details
+    add constraint sp_account_details_fk
+        foreign key ( account_id )
+            references wksp_wsdtbdev.sp_accounts ( id )
+                on delete cascade
+        enable;
+
